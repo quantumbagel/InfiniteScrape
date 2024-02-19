cde5c349d9446a446ca590b008cde471a9660243
quantumbagel <jjreder@gmail.com>
2024-02-19 13:51:24 -0500

    look ma, i got multithreading

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ proxies = []  # Will contain proxies [ip, port, parsed]
 
 
 requests_total = 0
-def craft(one, two):
+def craft(one, two, proxy):
     global requests_total
     global proxies
-    to_use = random.choice([p['parsed'] for p in proxies])
     headers = {
         'User-Agent': ua.random,
         'Accept': '*/*',
@@ -41,29 +40,45 @@ def craft(one, two):
         'first': one,
         'second': two,
     }
-    response = requests.get('https://neal.fun/api/infinite-craft/pair', params=params, headers=headers, proxies={"https": "socks5h://142.54.237.34:4145"}, verify=False)
+    response = requests.get('https://neal.fun/api/infinite-craft/pair', params=params, headers=headers, proxies={"https": proxy}, verify=False)
     string_response = response.content.decode('utf-8')  # Format byte response
     print("STR", string_response)
     try:
         json_resp = json.loads(string_response)
     except json.JSONDecodeError:
-        print("Ratelimit reached, restarting IP")
+        print("Ratelimit reached, restarting")
         return craft(one, two)
     requests_total += 1
     return json_resp
+
 
 def cpu_heavy(x, y):
     print('I am', x, y)
     time.sleep(10)
 
-def multiprocessing(func, args, workers):
+def handle(crafts, identification, proxy):
+    for c in crafts:
+        print("Worker", identification, "now crafting", c)
+        out = craft(c[0], c[1], proxy)
+        print("Worker", identification, "outputted", out)
+    print("Task concluded.")
+
+def scheduler(workers):
     with ProcessPoolExecutor(workers) as ex:
         combin = list(itertools.combinations(base_depth, 2))
         things_per = len(combin) // workers
         remaining_extra = len(combin) % workers
-        res = ex.map(func, args, )
-        ex.submit(func, args, )
-    return list(res)
+        start = 0
+        for worker_index in range(workers):
+            if (worker_index+1) <= remaining_extra:
+                ex.submit(handle, combin[start:start+things_per+1], worker_index, proxies[worker_index]['parsed'])
+                start += things_per + 1
+            else:
+                ex.submit(handle, combin[start:start+things_per], worker_index, proxies[worker_index]['parsed'])
+                start += things_per
+
+
+
 
 def evolve(word_set):
 
@@ -91,6 +106,7 @@ def evolve(word_set):
 base_depth = json.load(open('depth.json'))
 base_tree = json.load(open('tree.json'))
 
+
 # Retrieve latest proxies
 def update_proxies():
     """
@@ -108,6 +124,7 @@ def update_proxies():
     This is what you get.
     :return:
     """
+    proxies = []
     proxies_doc = requests.get('https://spys.one/en/socks-proxy-list', headers={"User-Agent": ua.random, "Content-Type": "application/x-www-form-urlencoded"}).text
     soup = BeautifulSoup(proxies_doc, 'html.parser')
     tables = list(soup.find_all("table"))  # Get ALL the tables
@@ -149,5 +166,9 @@ def update_proxies():
 # print(evolved)
 
 if __name__ == "__main__":
-    multiprocessing(cpu_heavy, range(50), 50)
+    proxies = update_proxies()
+    print(proxies)
+    scheduler(30)
+    while True:
+        continue
 
